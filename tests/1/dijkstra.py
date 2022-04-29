d9c1ba0a2596777ebff28b6b54fe34a5562dab85
Christopher Stewart <chris.stewart132@gmail.com>
2022-04-30 00:00:11 +1200

    added files for example network demonstration, dijkstra, some changes to validate packets (will manually add on sat to prevent merging)

---
+++ tests/1/dijkstra.py
@@ -0,0 +1,108 @@
+from pprint import pprint
+
+from collections import deque
+#insert, pop, popleft, remove, index, append
+
+
+from math import inf
+
+def adjacency_list(graph_str):
+    '''g'''
+    if not graph_str:
+        return []
+    if graph_str[-1] == "\n":
+        graph_str = graph_str[:-1]#remove last \n
+    data = graph_str.split("\n")
+    directed = data[0].split(' ')[0] == 'D'
+    n = int(data[0].split(' ')[1])
+    E = [x.split(" ") for x in data[1:]]
+    weighted = False
+    if E:
+        weighted = len(E[0]) > 2
+    
+    #print("D:", directed, "n:", n, "W:", weighted)
+    #print(E)
+
+    output = []
+    for i in range(n):
+        temp = []
+        for j in range(len(E)):
+            start_vertex = int(E[j][0])
+            end_vertex = int(E[j][1])
+            if start_vertex == i or end_vertex == i:
+                weight = None
+                if weighted:
+                    weight = int(E[j][2])
+                if directed and start_vertex == i:
+                    temp.append((end_vertex, weight))
+                elif start_vertex == i:
+                    temp.append((end_vertex, weight))
+                if not directed and end_vertex == i:
+                    temp.append((start_vertex, weight))
+        output.append(temp)
+    
+    return output
+
+
+def dijkstra(adj_list, start):
+    '''g'''
+    n = len(adj_list)
+    if n == 0:
+        return [], []
+    in_tree = [False for x in range(n)]
+    distance = [float('inf') for x in range(n)]
+    parent = [None for x in range(n)]
+    distance[start] = 0
+    while not all(in_tree):
+        u = next_vertex(in_tree, distance)
+        in_tree[u] = True
+        for v, weight in adj_list[u]:
+            if not in_tree[v] and (distance[u] + weight) < distance[v]:
+                distance[v] = distance[u] + weight
+                parent[v] = u
+                
+    return parent, distance
+
+
+def next_vertex(in_tree, distance):
+    '''g'''
+    d = None
+    for i, node_in_tree in enumerate(in_tree):
+        if not node_in_tree:
+            if d == None:
+                d = i
+            if distance[i] < distance[d]:
+                d = i
+    return d
+
+
+graph_string = """\
+U 8 W
+1 2 1
+1 7 8
+1 6 5
+2 1 1
+2 3 3
+3 2 3
+3 4 4
+4 3 4
+4 7 6
+4 5 2
+5 4 2
+5 6 1
+6 5 1
+6 1 5
+7 1 8
+7 4 6
+"""
+
+parent, cost = (dijkstra(adjacency_list(graph_string), 2))#forwarding table for x
+#print(parent[1:],cost[1:])#extra 0 vertex is included so must be removed
+print("parent (p) is the last hop to the target router addr (a)")
+print('a','p',"c (addr, parent(not next_hop), cost)")
+for i in range(1,len(cost)):
+    p = parent[i]
+    if p == None:
+        p = ' '
+    print(i,p,cost[i])
+
---
+++ tests/1/temp.py
@@ -0,0 +1,77 @@
+#just changed command to 2 instead of 1
+def create_response(self, destination):
+    '''
+    command(1) - version(1) - router_id(2)  #header(4)
+
+    addr_family_id(2) - zero(2)        #each entry (20)
+    ipv4_addr(4)
+    zero(4)
+    zero(4)
+    metric(4)
+    '''
+    command = int(2).to_bytes(1, 'big')#1 for req, 2 for response
+    version = int(2).to_bytes(1, 'big')
+    router_id = int(self.instance_id).to_bytes(2, 'big')
+    zero2 = int(0).to_bytes(2, 'big')
+
+    header = command + version + router_id#zero2 #header uses router_id instead of 16bit zero
+
+    payload = bytes()
+    for router_id in self.table.keys():#for each destination router_id
+        addr_family_id = int(2).to_bytes(2, 'big')#2 = AF_INET
+        ipv4_addr = int(router_id).to_bytes(4, 'big')#next_hop is the router sending the response packet
+        zero4 = int(0).to_bytes(4, 'big')
+
+        if self.table[router_id].next_hop == destination:
+            metric = int(16).to_bytes(4, 'big')
+        else:
+            metric = int(self.table[router_id].cost).to_bytes(4, 'big')#1-15
+
+        payload += addr_family_id + zero2 + ipv4_addr + zero4 + zero4 + metric
+    result = header + payload
+    return result
+
+
+
+def read_response(self,data):
+    '''convert the recvd packet to a table, returns rId(int), table(dict)'''
+    command = data[0]
+    version = data[1]
+    if command != 2 or version !=2:
+        return False,0,0#command or version value is incorrect
+    
+    router_id = int.from_bytes(data[2:4], 'big')#router(id) that sent the data
+   
+    i = 4#packet payload (RIP entries) starts after 4 bytes   
+    if (len(data)-4) % 20 != 0:
+        return False,0,0#data length incorrect (should be 4 + 20x)
+    
+    recvd_table = {}
+    while i < len(data):
+        try:
+            zeros = []#append all expected zero values here to validate packet
+            addr_family_id = int.from_bytes(data[i:i+2], 'big')
+            i+=2
+            zeros.append(int.from_bytes(data[i:i+2], 'big'))#zero2
+            i+=2
+            ipv4_addr = int.from_bytes(data[i:i+4], 'big')#dest addr from the router sending
+            i+=4
+            zeros.append(int.from_bytes(data[i:i+4], 'big'))#zero4
+            i+=4
+            zeros.append(int.from_bytes(data[i:i+4], 'big'))#zero4
+            i+=4
+            metric = int.from_bytes(data[i:i+4], 'big')# between 1-5 (inclusive) or 16 (inf)
+            row = Row(metric, router_id)#cost, next_hop
+            recvd_table[ipv4_addr] = row
+            i+=4
+
+            if min(zeros) != 0 or max(zeros) != 0 or metric < 1 or metric > 16:
+                return False,0,0#bad RIP entry            
+        except IndexError:
+            return False,0,0#data length incorrect (should be 4 + 20x)
+    return True, router_id, recvd_table
+
+#in run function
+packet_valid, other_router_id, other_table = self.read_response(data)
+if packet_valid:
+    self.update_table(other_router_id, other_table)
