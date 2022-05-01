87da6e17ce7c75accf40f4a043ae34e18b12eb9b
Frederik Markwell <fma107@uclive.ac.nz>
2022-05-01 14:57:35 +1200

    Added shell script to start up all routers with example

---
+++ example.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+gnome-terminal -- python3 ripd.py tests/1/config1.txt
+gnome-terminal -- python3 ripd.py tests/1/config2.txt
+gnome-terminal -- python3 ripd.py tests/1/config3.txt
+gnome-terminal -- python3 ripd.py tests/1/config4.txt
+gnome-terminal -- python3 ripd.py tests/1/config5.txt
+gnome-terminal -- python3 ripd.py tests/1/config6.txt
+gnome-terminal -- python3 ripd.py tests/1/config7.txt
